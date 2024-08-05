739efd3a45836acce33b9c58eab188a6f0f8b01c
Deva D <somehaters@gmail.com>
2024-08-05 14:45:09 -0500

    Starting the project off with React-router-dom v6. Planning for design still under way

---
+++ src/pages/Main.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+const MainPage = () =>{
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return(
+        <div>
+            Main
+            Main
+            Main
+            Main
+            Main
+        </div>
+    )
+}
+export default MainPage;
